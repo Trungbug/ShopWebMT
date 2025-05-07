1c269631bd5736a533c99e5b37092e83d227c2cf
Trungbug <baotrung2032608@gmail.com>
2025-05-07 23:00:02 +0700

    user

---
+++ src/main/java/com/example/demo/enity/AppRole.java
@@ -0,0 +1,7 @@
+package com.example.demo.enity;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_SELLER,
+    ROLE_ADMIN
+}
--- src/main/java/com/example/demo/enity/Role.java
+++ src/main/java/com/example/demo/enity/Role.java
@@ -1,4 +1,26 @@
 package com.example.demo.enity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @ToString.Exclude
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+
 }
